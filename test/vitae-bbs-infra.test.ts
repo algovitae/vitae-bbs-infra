77cc6aba567fcd5e69ea1e7f97c593cc4f0c278a
algovitae <algovitae@zohomail.com>
2022-05-22 10:41:17 +0000

    cdk init

---
+++ test/vitae-bbs-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VitaeBbsInfra from '../lib/vitae-bbs-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vitae-bbs-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VitaeBbsInfra.VitaeBbsInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
