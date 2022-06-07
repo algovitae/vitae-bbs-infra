839b0c82dc59b080614943619a8a1def4d9348ee
algovitae <algovitae@zohomail.com>
2022-06-07 12:35:55 +0000

    fix

--- bin/vitae-bbs-infra.ts
+++ bin/vitae-bbs-infra.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { VitaeBbsInfraStack } from '../lib/vitae-bbs-infra-stack';
 import { VitaeBbsDynamoDbStack } from '../lib/vitae-bbs-dynamodb-stack';
 
 const app = new cdk.App();
